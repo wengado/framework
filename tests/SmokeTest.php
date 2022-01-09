6431dbe465644aed6e079ec0193f2df4cce7138d
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2022-01-09 13:18:37 +0100

    Added version test

--- tests/SmokeTest.php
+++ tests/SmokeTest.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace Wengado\Tests;
 
+use Wengado\Application;
+
 class SmokeTest extends TestCase
 {
     /** @test */
     public function it_looks_like_a_real_test()
     {
         $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function it_is_the_latest_version()
+    {
+        $this->assertEquals('2.9', Application::VERSION);
     }
 }
